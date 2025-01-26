d1f9a6f87af243d0be8b71e73e4286d3de72f5b5
Suheer Khan <khansuheer1@gmail.com>
2025-01-26 20:58:48 +0500

    Solved Power

---
+++ Practice/Recursion/Power/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+//loop to recursion conversion method
+int power(int base, int exp, int result,int i = 1){
+    if( i >= exp){
+        return result;
+    }
+
+    return power(base, exp, base * result, i+1);
+}
+
+//direct implementation
+int power1(int base, int exp){
+    if(exp == 0){
+        return 1;
+    }
+
+    return base * power1(base, exp-1);
+}
+
+
+int main(){
+    while (true)
+    {
+    
+        int base, exp;
+
+        cout<<"Enter Base: ";
+        cin>>base;
+
+        cout<<"Enter Exponent: ";
+        cin>>exp;
+
+        cout<<power1(base, exp)<<endl;
+
+    }
+    return 0;
+}
