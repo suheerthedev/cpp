8fdd9256676ca1a04080357ba27c0375ce598023
Suheer Khan <khansuheer1@gmail.com>
2025-02-14 08:35:25 +0500

    Implemented more efficient solution for Sum of Array

--- Practice/Recursion/Sum of Array/main.cpp
+++ Practice/Recursion/Sum of Array/main.cpp
@@ -9,11 +9,18 @@ int sumOfArray(int arr[],int size, int i = 0){
     return arr[i] + sumOfArray(arr, size, i+1);
 }
 
+int sumArr(int arr[], int size){
+    if(size == 0){
+        return 0;
+    }
+    return arr[size-1]+ sumArr(arr,size-1);
+}
+
 int main(){
     int size = 4;
     int arr[size] = {1,2,3,4};
 
-    cout<<sumOfArray(arr,size);
+    cout<<sumArr(arr,size);
 
     return 0;
 }
