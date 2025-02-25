d442d685e6de1b2fa1e5c17f869ebc373b9bb0dc
Suheer Khan <khansuheer1@gmail.com>
2025-02-25 13:27:01 +0500

    Completed Class Task

--- OOP/Class Code/Class 1/main.cpp
+++ OOP/Class Code/Class 1/main.cpp
@@ -6,13 +6,30 @@ class MyClass
 {
 public:
     string name;
+    void percentage();
 };
 
-int main(){
-    MyClass c1;
-    c1.name = "Suheer";
+class Room{
+    public:
+        float length;
+        float breadth;
+        float height;
+        float calculateArea(){
+            return length*breadth;
+        }
+        float calculateVolume(){
+            return length*breadth*height;
+        }
+};
 
-    cout<<c1.name<<endl;
+
+int main(){
+    Room r1;
+    r1.height = 10;
+    r1.breadth= 10;
+    r1.length = 10;
+
+    cout<<r1.calculateArea()<<endl;
     return 0;
 }
 
